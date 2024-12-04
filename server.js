55bed5efedd519a442b34e62a2c7b5755871b868
javierpartida02 <a21490537@itmexicali.edu.mx>
2024-12-04 14:50:53 -0800

    Actualicé la configuración para conectar con Railway

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
+    port: 51407
 });
  
 // Conectar a la base de datos 
