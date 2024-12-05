262377aa472d1de7844b68ec7027435a62ddf5e1
javierpartida02 <a21490537@itmexicali.edu.mx>
2024-12-04 17:25:13 -0800

    Actualicé la configuración para conectar con Railway

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
-const PORT= process.env.PORT || 4000
+const PORT = process.env.PORT || 51407;
 const DB_HOST = process.env.DB_HOST || "autorack.proxy.rlwy.net";
-const DB_USER= process.env.DB_USER || "root";
-const DB_PASSWORD= process.env.DB_PASSWORD || "trhKjygWDnafQSMqykOkifLeAmhZThwL";
-const DB_DATABASE= process.env.DB_DATABASE || "railway";
-
-
+const DB_USER = process.env.DB_USER || "root";
+const DB_PASSWORD = process.env.DB_PASSWORD || "trhKjygWDnafQSMqykOkifLeAmhZThwL";
+const DB_DATABASE = process.env.DB_DATABASE || "railway";
 
 module.exports = {
     PORT,
@@ -12,5 +10,4 @@ module.exports = {
     DB_USER,
     DB_PASSWORD,
     DB_DATABASE
-
-}
+};
