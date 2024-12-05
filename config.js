ddb85cf44be7fe81930636a72448a59e11662c04
javierpartida02 <a21490537@itmexicali.edu.mx>
2024-12-04 17:48:03 -0800

    Actualicé la configuración para conectar con Railway

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const PORT = process.env.PORT || 51407;
-const DB_HOST = process.env.DB_HOST || "mysql.railway.internal";
+const DB_HOST = process.env.DB_HOST || "autorack.proxy.rlwy.net";
 const DB_USER = process.env.DB_USER || "root";
 const DB_PASSWORD = process.env.DB_PASSWORD || "trhKjygWDnafQSMqykOkifLeAmhZThwL";
 const DB_DATABASE = process.env.DB_DATABASE || "railway";
+connectTimeout: 10000 // Tiempo en milisegundos
 
 module.exports = {
     PORT,
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = 51407;
+const port = 4000;
 const mysql = require('mysql2');
 
 
@@ -20,6 +20,7 @@ const db = mysql.createConnection({
     password: DB_PASSWORD,
     database: DB_DATABASE,
     port: PORT
+    
 });
 
 // Conectar a la base de datos 
