d90400e4f7bc39024cf3cc235eb553616eb6c644
javierpartida02 <a21490537@itmexicali.edu.mx>
2024-12-05 17:43:21 -0800

    Actualicé la configuración para conectar con Railway

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 51407;
+const port = process.env.PORT;
 const mysql = require('mysql2');
 
 
